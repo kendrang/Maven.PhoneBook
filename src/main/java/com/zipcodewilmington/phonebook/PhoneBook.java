f898dbaa48b45574738d2ea832ff45b96ce22c73
kendrang <kendrang@users.noreply.github.com>
2019-11-08 18:16:06 -0500

    going to researched linked list

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,45 +1,59 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+    private Map<String, List<String>> myMap;
+
     public PhoneBook(Map<String, List<String>> map) {
+
+        this.myMap = map;
     }
 
     public PhoneBook() {
-        this(null);
+         this.myMap = new LinkedHashMap<>();
     }
 
     public void add(String name, String phoneNumber) {
+        ArrayList<String> numbers = new ArrayList<>();
+        numbers.add(phoneNumber);
+        myMap.put(name,numbers);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        myMap.put(name, Arrays.asList(phoneNumbers));
     }
 
     public void remove(String name) {
+        myMap.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+
+        return myMap.containsKey(name);
     }
 
     public List<String> lookup(String name) {
+
         return null;
     }
 
     public String reverseLookup(String phoneNumber)  {
+
         return null;
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        ArrayList<String> contacts = new ArrayList<>(myMap.keySet());
+        return contacts;
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+
+        return myMap;
     }
 }
