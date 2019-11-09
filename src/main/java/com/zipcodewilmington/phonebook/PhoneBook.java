48edd63cf05e78a8902665aab5cd24652347084b
kendrang <kendrang@users.noreply.github.com>
2019-11-09 11:09:26 -0500

    done haha

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -15,13 +15,13 @@ public class PhoneBook {
     }
 
     public PhoneBook() {
-         this.myMap = new LinkedHashMap<>();
+        this.myMap = new LinkedHashMap<>();
     }
 
     public void add(String name, String phoneNumber) {
         ArrayList<String> numbers = new ArrayList<>();
         numbers.add(phoneNumber);
-        myMap.put(name,numbers);
+        myMap.put(name, numbers);
     }
 
     public void addAll(String name, String... phoneNumbers) {
@@ -33,18 +33,23 @@ public class PhoneBook {
     }
 
     public Boolean hasEntry(String name) {
-
         return myMap.containsKey(name);
     }
 
+
     public List<String> lookup(String name) {
 
-        return null;
+        return myMap.get(name);
     }
 
-    public String reverseLookup(String phoneNumber)  {
-
-        return null;
+    public String reverseLookup(String phoneNumber) {
+        String result = "";
+        for (Map.Entry<String, List<String>> entry : myMap.entrySet()) {
+            if (entry.getValue().contains(phoneNumber)) {
+                result = entry.getKey();
+            }
+        }
+        return result;
     }
 
     public List<String> getAllContactNames() {
